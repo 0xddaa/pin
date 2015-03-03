0bf462fc307adaaa08c169ac8d9e4ca2e1c94b4b
chengtc <chengtc@cs.nctu.edu.tw>
2015-03-03 16:10:06 +0800

    modify script to control log name

--- CodeCoverage/run.sh
+++ CodeCoverage/run.sh
@@ -7,4 +7,10 @@ fi
 
 make clean
 make
-sudo pin -t obj-ia32/codecoverage.so -o "log/$1_"`date +"%T_%m-%d"` -- $1
+
+if [ -z $2 ]; then
+    sudo pin -t obj-ia32/codecoverage.so -o "log/$1_"`date +"%T_%m-%d"` -- $1
+else
+    sudo pin -t obj-ia32/codecoverage.so -o "log/"$2 -- $1
+fi
+    
