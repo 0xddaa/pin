ec60535623731ad8a22818986fcb0b9130f63e6b
chengtc <chengtc@cs.nctu.edu.tw>
2015-03-06 01:17:05 +0800

    show json log in the end

--- CodeCoverage/run.sh
+++ CodeCoverage/run.sh
@@ -5,13 +5,15 @@ if [ -z $1 ]; then
     exit
 fi
 
-make clean
 make
 
 if [ -z $2 ]; then
     sudo pin -t obj-ia32/codecoverage.so -o "log/$1_"`date +"%T_%m-%d"` -- $1
 else
     sudo pin -t obj-ia32/codecoverage.so -o "log/"$2 -- $1
+    cat "log/"$2 | python -m json.tool | less
 fi
     
-sudo chmod 644 pintool.log
+if [ -e pintool.log ]; then
+    sudo chmod 644 pintool.log
+fi
