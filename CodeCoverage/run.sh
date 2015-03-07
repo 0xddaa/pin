cec36373b11f6a55528c9dca08f1ac2196e508c0
chengtc <chengtc@cs.nctu.edu.tw>
2015-03-07 17:00:50 +0800

    no need root

--- CodeCoverage/run.sh
+++ CodeCoverage/run.sh
@@ -8,12 +8,8 @@ fi
 make
 
 if [ -z $2 ]; then
-    sudo pin -t obj-ia32/codecoverage.so -o "log/$1_"`date +"%T_%m-%d"` -- $1
+    pin -t obj-ia32/codecoverage.so -o "log/$1_"`date +"%T_%m-%d"` -- $1
 else
-    sudo pin -t obj-ia32/codecoverage.so -o "log/"$2 -- $1
+    pin -t obj-ia32/codecoverage.so -o "log/"$2 -- $1
     cat "log/"$2 | python -m json.tool | less
-fi
-    
-if [ -e pintool.log ]; then
-    sudo chmod 644 pintool.log
 fi
